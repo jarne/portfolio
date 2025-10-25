c8a7af8dcae9d1799447e7c790768e803defbb7e
Jarne <dev@jarne.rocks>
2025-10-25 16:59:16 +0200

    Move project showcase into its own component, set showcase items to center

---
+++ src/components/showcase/projectShowcase.tsx
@@ -0,0 +1,104 @@
+/**
+ * Dev portfolio | project showcase component
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage, type IGatsbyImageData } from "gatsby-plugin-image"
+
+type Project = {
+    name: string
+    description: string
+    license: string
+    screenshot: string
+    languages: string[]
+    frameworks: string[]
+    tools: string[]
+    urls: {
+        source: string
+    }
+}
+type ProjectsEdge = {
+    node: Project
+}
+type DataQuery = {
+    allProjectsJson: {
+        edges: ProjectsEdge[]
+    }
+    allFile: {
+        edges: {
+            node: {
+                name: string
+                childImageSharp: {
+                    gatsbyImageData: IGatsbyImageData
+                }
+            }
+        }[]
+    }
+}
+
+const ProjectShowcase = () => {
+    const data = useStaticQuery<DataQuery>(graphql`
+        query ProjectDetailsQuery {
+            allProjectsJson {
+                edges {
+                    node {
+                        name
+                        description
+                        license
+                        screenshot
+                        languages
+                        frameworks
+                        tools
+                        urls {
+                            source
+                        }
+                    }
+                }
+            }
+            allFile(
+                filter: { relativePath: { regex: "/project-screenshots//" } }
+            ) {
+                edges {
+                    node {
+                        name
+                        childImageSharp {
+                            gatsbyImageData(
+                                width: 960
+                                placeholder: BLURRED
+                                formats: [AUTO, WEBP]
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    const renderProjectScreenshot = (
+        screenshotName: string,
+    ): React.ReactNode | null => {
+        const fileEdge = data.allFile.edges.find(
+            edge => edge.node.name === screenshotName,
+        )
+        return fileEdge ? (
+            <GatsbyImage
+                image={fileEdge.node.childImageSharp.gatsbyImageData}
+                className="border border-1 rounded"
+                alt="Project screenshot"
+            />
+        ) : null
+    }
+
+    return data.allProjectsJson.edges.map(
+        ({ node }: ProjectsEdge, index: number) => (
+            <section key={index}>
+                {renderProjectScreenshot(node.screenshot)}
+                <h2>{node.name}</h2>
+                <p>{node.description}</p>
+            </section>
+        ),
+    )
+}
+
+export default ProjectShowcase
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,111 +3,23 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import type { HeadFC, PageProps } from "gatsby"
-import { GatsbyImage, type IGatsbyImageData } from "gatsby-plugin-image"
 
 import ProfileHeader from "../components/header/profileHeader"
+import ProjectShowcase from "../components/showcase/projectShowcase"
+import { showcaseContainer } from "./index.module.css"
 
 import "../utils/bootstrapImport.scss"
 
-type Project = {
-    name: string
-    description: string
-    license: string
-    screenshot: string
-    languages: string[]
-    frameworks: string[]
-    tools: string[]
-    urls: {
-        source: string
-    }
-}
-type ProjectsEdge = {
-    node: Project
-}
-type DataQuery = {
-    allProjectsJson: {
-        edges: ProjectsEdge[]
-    }
-    allFile: {
-        edges: {
-            node: {
-                name: string
-                childImageSharp: {
-                    gatsbyImageData: IGatsbyImageData
-                }
-            }
-        }[]
-    }
-}
-
 const IndexPage: React.FC<PageProps> = () => {
-    const data = useStaticQuery<DataQuery>(graphql`
-        query ProjectDetailsQuery {
-            allProjectsJson {
-                edges {
-                    node {
-                        name
-                        description
-                        license
-                        screenshot
-                        languages
-                        frameworks
-                        tools
-                        urls {
-                            source
-                        }
-                    }
-                }
-            }
-            allFile(
-                filter: { relativePath: { regex: "/project-screenshots//" } }
-            ) {
-                edges {
-                    node {
-                        name
-                        childImageSharp {
-                            gatsbyImageData(
-                                width: 960
-                                placeholder: BLURRED
-                                formats: [AUTO, WEBP]
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    `)
-
-    const renderProjectScreenshot = (
-        screenshotName: string,
-    ): React.ReactNode | null => {
-        const fileEdge = data.allFile.edges.find(
-            edge => edge.node.name === screenshotName,
-        )
-        return fileEdge ? (
-            <GatsbyImage
-                image={fileEdge.node.childImageSharp.gatsbyImageData}
-                alt="Project screenshot"
-            />
-        ) : null
-    }
-
     return (
         <main className="container">
             <ProfileHeader />
-            <ul>
-                {data.allProjectsJson.edges.map(
-                    ({ node }: ProjectsEdge, index: number) => (
-                        <li key={index}>
-                            <h2>{node.name}</h2>
-                            <p>{node.description}</p>
-                            {renderProjectScreenshot(node.screenshot)}
-                        </li>
-                    ),
-                )}
-            </ul>
+            <div className="d-flex justify-content-center">
+                <div className={showcaseContainer}>
+                    <ProjectShowcase />
+                </div>
+            </div>
         </main>
     )
 }
