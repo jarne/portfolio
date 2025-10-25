d50abb7e63a815e53d8bbe7faead39f336590c33
Jarne <dev@jarne.rocks>
2025-10-25 14:39:28 +0200

    Create data workflow from JSON files for showcase projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,47 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import type { HeadFC, PageProps } from "gatsby"
 
+type Project = {
+    name: string
+    description: string
+}
+type ProjectsEdge = {
+    node: Project
+}
+type DataQuery = {
+    allProjectsJson: {
+        edges: ProjectsEdge[]
+    }
+}
+
 const IndexPage: React.FC<PageProps> = () => {
+    const data = useStaticQuery<DataQuery>(graphql`
+        query ProjectDetailsQuery {
+            allProjectsJson {
+                edges {
+                    node {
+                        name
+                        description
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <main>
             <h1>Test 123</h1>
+            <ul>
+                {data.allProjectsJson.edges.map(
+                    ({ node }: ProjectsEdge, index: number) => (
+                        <li key={index}>
+                            <h2>{node.name}</h2>
+                            <p>{node.description}</p>
+                        </li>
+                    ),
+                )}
+            </ul>
         </main>
     )
 }
