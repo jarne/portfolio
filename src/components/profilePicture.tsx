21dbcc097f2e7e84b11673210984e2d2e9ba9062
Jarne <dev@jarne.rocks>
2025-10-25 15:11:45 +0200

    Add description comments to files

--- src/components/profilePicture.tsx
+++ src/components/profilePicture.tsx
@@ -1,3 +1,7 @@
+/**
+ * Dev portfolio | rounded profile picture component
+ */
+
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+/**
+ * Dev portfolio | main content page
+ */
+
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import type { HeadFC, PageProps } from "gatsby"
